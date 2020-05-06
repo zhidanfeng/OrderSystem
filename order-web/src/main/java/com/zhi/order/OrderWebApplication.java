08772b509d52dc3329a7a643a0ce916cbd364e2a
zhidanfeng <18681491656@163.com>
2020-05-06 17:49:42 +0800

    Initial commit

---
+++ order-web/src/main/java/com/zhi/order/OrderWebApplication.java
@@ -0,0 +1,13 @@
+package com.zhi.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderWebApplication.class, args);
+    }
+
+}
